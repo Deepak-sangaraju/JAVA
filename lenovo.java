65fcd29fd4d33bf9e3c3782e5137e7c9878166e6
Deepak-sangaraju <sreddydeepak2006@gmail.com>
2025-08-28 12:18:42 +0530

    Add files via upload

---
+++ lenovo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class lenovo{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the two number : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b){
+            System.out.println("a is greater then b");        }
+        else {
+            System.out.println("b is greater than a ");
+        }
+    }
+}
