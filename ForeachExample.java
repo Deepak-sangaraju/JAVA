1ee36392220fa10a1bd322d6ff51b7ff4b3a5f52
Deepak-sangaraju <sreddydeepak2006@gmail.com>
2025-09-23 15:04:28 +0530

    Add files via upload

---
+++ ForeachExample.java
@@ -0,0 +1,8 @@
+public class ForeachExample {
+    public static void main(String[] args){
+        String[] fruits={"Apple","banana","Cherry"};
+        for(String fruit:fruits){
+            System.out.println("Fruit:"+fruit);
+        }
+    }
+}
